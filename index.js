9598d35bdfa4e5edead02cd4153430afdf9e53c9
m16khb <m16khb@gmail.com>
2022-06-13 12:39:49 +0900

    first push

---
+++ index.js
@@ -0,0 +1,9 @@
+var 이름 = '김하빈';
+var 나이 = 27;
+var 고향 = '인천';
+var 노래 = { 곡: '잘됐으면좋겠다', 가수: '홍대광' };
+var project = {
+    member: ['kim', 'park'],
+    days: 30,
+    started: true,
+};
---
+++ index.ts
@@ -0,0 +1,9 @@
+let 이름: string = '김하빈';
+let 나이: number = 27;
+let 고향: string = '인천';
+let 노래: { 곡: string; 가수: string } = { 곡: '잘됐으면좋겠다', 가수: '홍대광' };
+let project: { member: string[]; days: number; started: boolean } = {
+	member: ['kim', 'park'],
+	days: 30,
+	started: true,
+};
