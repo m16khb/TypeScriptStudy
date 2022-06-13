2ef76e332054949a106202f20a14ad2dbbd46ee0
m16khb <m16khb@gmail.com>
2022-06-13 22:56:10 +0900

    unknown / any / union type

--- index.ts
+++ index.ts
@@ -7,3 +7,23 @@ let project: { member: string[]; days: number; started: boolean } = {
 	days: 30,
 	started: true,
 };
+let 회원: number | string = 123;
+회원 = 'vip';
+let 회원들: (number | string)[] = [1, '2', 3];
+let 오브젝트: { a: string | number } = { a: '123' };
+오브젝트.a = 123;
+
+/*
+let 이름2: any;
+이름2 = 123;
+이름2 = true;
+*/
+
+//unknown의 경우 any보다 타입에 대해 안전하다.
+let 이름2: unknown;
+이름2 = 123;
+이름2 = {};
+//let 변수1: string = 이름2;
+//이름 - 1;
+let 나이2: string | number = 1;
+나이2 = 나이2 + 1;
